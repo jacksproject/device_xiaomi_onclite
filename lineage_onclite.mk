db244fb5ab0b4515c56f2ab89ea93a2774f49426
jacksproject <contact@jackbd.me>
2022-04-06 18:11:45 +0600

    Bringup CrDroid

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -12,12 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_onclite
+PRODUCT_NAME := lineage_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
@@ -29,12 +29,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=JackTheRipper
-
-# CherishOS Official
-CHERISH_BUILD_TYPE := OFFICIAL
-
-# Disable Live Wallpaper
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
