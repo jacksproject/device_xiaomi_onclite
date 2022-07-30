cd51af1fb4763968ad7513067f7bf591e3ee44d5
jacksproject <contact@jackbd.me>
2022-07-30 11:24:31 +0600

    onclite: Include vendorsetup.sh To save times, no need to clone manually Lazy moment!

---
+++ vendorsetup.sh
@@ -0,0 +1,17 @@
+# Remove Default Hals
+rm -rf hardware/qcom-caf/wlan
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996/display
+
+# Clone Hals
+git clone https://github.com/bibarub/hardware_qcom_wlan -b 12.x hardware/qcom-caf/wlan
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-19.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-19.1-caf-msm8996 hardware/qcom-caf/msm8996/media
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-19.1-caf-msm8996 hardware/qcom-caf/msm8996/display
+
+# Kernel
+git clone https://github.com/LineageOS/android_kernel_xiaomi_onclite -b lineage-19.1 kernel/xiaomi/onclite
+
+# Vendor
+git clone https://github.com/jacksproject/android_vendor_xiaomi_onclite -b twelve vendor/xiaomi/onclite
